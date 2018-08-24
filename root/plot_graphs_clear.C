f0520be97332b509eaa2397cafdc5a292c2546e2
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2018-08-24 18:26:19 +0900

    plot_graphs_clear.C

---
+++ root/plot_graphs_clear.C
@@ -0,0 +1,24 @@
+void plot_graphs_clear(){
+  TCanvas* canvas = gPad->GetCanvas();
+  if (canvas == 0) {
+    std::cout << "There is no canvas." << std::endl;
+    return;
+  }
+  TVirtualPad *sel_pad = canvas->GetPad(gPad->GetNumber());
+  if (sel_pad == 0) {
+    std::cout << "There is no sel_pad." << std::endl;
+    return;
+  }
+  TList *listofpri = sel_pad->GetListOfPrimitives();
+  if (listofpri == 0) {
+    std::cout << "The pad includes nothing." << std::endl;
+    return;
+  }
+  Int_t ngraphs = 0;
+  TGraph *gr;
+  while(gr = (TGraph*)listofpri->FindObject(Form("Graph_%d",ngraphs))){
+    ngraphs++;
+    gr->Delete();
+  }
+  canvas->Update();
+}
