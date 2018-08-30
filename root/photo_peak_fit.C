f279eaa5c3d0d1073af575f2d0756692e1438f51
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2018-08-30 14:16:38 +0900

    First root/photo_peak_fit.C was made.

---
+++ root/photo_peak_fit.C
@@ -0,0 +1,378 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include "TMath.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TPad.h"
+#include "TList.h"
+#include "TString.h"
+#include "TVirtualFitter.h"
+#include "TFitResult.h"
+
+Double_t photo_peak_fit_PolyBg(Double_t *dim, Double_t *par, Int_t order) {
+  Double_t result = 0.0;
+  int j=0;
+  for(Int_t i=0;i<=order;i++) {
+    result += *(par+j) *TMath::Power(dim[0],i);
+    j++;
+  }
+  //result += par[i]*TMath::Power(dim[0]-par[order+1],i);
+  return result;
+}
+
+Double_t photo_peak_fit_StepFunction(Double_t *dim, Double_t *par) {
+  //  -dim[0]: channels to fit
+  //  -par[0]: height of peak
+  //  -par[1]: centroid of peak
+  //  -par[2]: sigma of peak
+  //  -par[3]: size of step in step function.
+
+  Double_t x       = dim[0];
+  
+  Double_t height  = par[0];
+  Double_t cent    = par[1];
+  Double_t sigma   = par[2];
+  //Double_t R       = par[4];
+  Double_t step    = par[3];
+
+  //return TMath::Abs(step)*height/100.0*TMath::Erfc((x-cent)/(TMath::Sqrt(2.0)*sigma));
+  return height*(step/100.0) *TMath::Erfc((x-cent)/(TMath::Sqrt(2.0)*sigma));
+}
+
+Double_t photo_peak_fit_StepBG(Double_t *dim, Double_t *par) {
+  return photo_peak_fit_StepFunction(dim,par) + photo_peak_fit_PolyBg(dim,(par+4),0);
+}
+
+Double_t photo_peak_fit_Gaus(Double_t *dim, Double_t *par) {
+  // - dim[0]: channels to fit
+  // - par[0]: height of peak
+  // - par[1]: cent of peak
+  // - par[2]: sigma
+  // - par[3]: relative height of skewed gaus to gaus
+
+  Double_t x      = dim[0];
+  Double_t height = par[0];
+  Double_t cent   = par[1];
+  Double_t sigma  = par[2];
+  Double_t R      = par[3];
+
+  return height*(1.0-R/100.0)*TMath::Gaus(x,cent,sigma);
+}
+
+Double_t photo_peak_fit_SkewedGaus(Double_t *dim,Double_t *par) {
+  // StepFunction(dim,par) + PolyBg
+  // - par[0]: height of peak
+  // - par[1]: cent of peak
+  // - par[2]: sigma
+  // - par[3]: relative height of skewed gaus to gaus
+  // - par[4]: "skewedness" of the skewed gaussin
+
+  Double_t x      = dim[0]; //channel number used for fitting
+  Double_t height = par[0]; //height of photopeak
+  Double_t cent   = par[1]; //Peak Centroid of non skew gaus
+  Double_t sigma  = par[2]; //standard deviation of gaussian
+  Double_t R      = par[3]; //relative height of skewed gaussian
+  Double_t beta   = par[4]; //"skewedness" of the skewed gaussian
+
+  double scaling = R*height/100.0;
+  //double x_rel = (x - cent)/sigma;
+
+  double fterm = (x-cent)/(sigma*TMath::Sqrt(2.));
+  double sterm = sigma /  (beta *TMath::Sqrt(2.));
+
+  return scaling * TMath::Exp((x-cent)/beta) * TMath::Erfc(fterm + sterm); 
+}
+
+Double_t photo_peak_fit_PhotoPeak(Double_t *dim,Double_t *par) {
+  return photo_peak_fit_Gaus(dim,par) + photo_peak_fit_SkewedGaus(dim,par);
+}
+
+Double_t photo_peak_fit_PhotoPeakBG(Double_t *dim,Double_t *par) {
+  // - dim[0]: channels to fit
+  // - par[0]: height of peak
+  // - par[1]: cent of peak
+  // - par[2]: sigma
+  // - par[3]: relative height of skewed gaus to gaus
+  // - par[4]: "skewedness" of the skewed gaussin
+  // - par[5]: size of stepfunction step.
+
+  // - par[6]: base bg height.
+  // - par[7]: slope of bg.
+  
+  double spar[4];
+  spar[0] = par[0];
+  spar[1] = par[1];
+  spar[2] = par[2];
+  spar[3] = par[5];  //stepsize;
+  return photo_peak_fit_Gaus(dim,par) + photo_peak_fit_SkewedGaus(dim,par) + photo_peak_fit_StepFunction(dim,spar) + photo_peak_fit_PolyBg(dim,par+6,0);
+}
+
+void photo_peak_fit_InitParams(TH1 *fithist, TF1 *fitfunc){
+  if(!fithist){
+    printf("No histogram is associated yet, no initial guesses made\n");
+    return;
+  }
+  //printf("%s called.\n",__PRETTY_FUNCTION__); fflush(stdout);
+  //Makes initial guesses at parameters for the fit. Uses the histogram to
+  Double_t xlow,xhigh;
+  fitfunc->GetRange(xlow,xhigh);
+
+  //Int_t bin = fithist->GetXaxis()->FindBin(GetParameter("centroid"));
+  Int_t binlow = fithist->GetXaxis()->FindBin(xlow);
+  Int_t binhigh = fithist->GetXaxis()->FindBin(xhigh);
+
+  Double_t highy  = fithist->GetBinContent(binlow);
+  Double_t lowy   = fithist->GetBinContent(binhigh);
+  for(int x=1;x<5;x++) {
+    highy += fithist->GetBinContent(binlow-x);
+    lowy  += fithist->GetBinContent(binhigh+x);
+  }
+  highy = highy/5.0;
+  lowy = lowy/5.0;
+
+//  Double_t yhigh  = fithist->GetBinContent(binhigh);
+//  Double_t ylow   = fithist->GetBinContent(binlow);
+  if(lowy>highy){
+    //std::swap(lowy,highy);
+    Double_t tmp = lowy;
+    lowy = highy;
+    highy = tmp;
+  }
+
+  double largestx=0.0;
+  double largesty=0.0;
+  int i = binlow;
+  for(;i<=binhigh;i++) {
+    if(fithist->GetBinContent(i) > largesty) {
+      largesty = fithist->GetBinContent(i);
+      largestx = fithist->GetXaxis()->GetBinCenter(i);
+    }
+  }
+
+  // - par[0]: height of peak
+  // - par[1]: cent of peak
+  // - par[2]: sigma
+  // - par[3]: R:    relative height of skewed gaus to gaus
+  // - par[4]: beta: "skewedness" of the skewed gaussin
+  // - par[5]: step: size of stepfunction step.
+
+  // - par[6]: base bg height.
+
+  //limits.
+  fitfunc->SetParLimits(0,0,largesty*2);
+  fitfunc->SetParLimits(1,xlow,xhigh);
+  fitfunc->SetParLimits(2,0.1,xhigh-xlow);
+  fitfunc->SetParLimits(3,0.0,40);
+  fitfunc->SetParLimits(4,0.01,5);
+  //double step = ((highy-lowy)/largesty)*50;
+  double step = (highy-lowy)/largesty*50;
+
+  //TF1::SetParLimits(5,step-step*.1,step+.1*step);
+  //printf(" highy = %.02f \t lowy = %.02f \t step = %.02f\n",highy,lowy,step); fflush(stdout);
+  
+  fitfunc->SetParLimits(0,0,largesty*2);
+  fitfunc->SetParLimits(1,xlow,xhigh);
+  fitfunc->SetParLimits(2,0.1,xhigh-xlow);
+  fitfunc->SetParLimits(3,0.0,40);
+  fitfunc->SetParLimits(5,0.0,step+step);
+
+  //double slope  = (yhigh-ylow)/(xhigh-xlow);
+  //double offset = yhigh-slope*xhigh;
+  double offset = lowy;
+  fitfunc->SetParLimits(6,offset-0.5*offset,offset+offset);
+  //TF1::SetParLimits(7,-2*slope,2*slope);
+
+  //Make initial guesses
+  fitfunc->SetParameter(0,largesty);         //fithist->GetBinContent(bin));
+  fitfunc->SetParameter(1,largestx);         //GetParameter("centroid"));
+  fitfunc->SetParameter(2,(largestx*.01)/2.35);                    //2,(xhigh-xlow));     //2.0/binWidth); //
+  fitfunc->SetParameter(3,5.);
+  fitfunc->SetParameter(4,1.);
+  fitfunc->SetParameter(5,step);
+  fitfunc->SetParameter(6,offset);
+  //TF1::SetParameter(7,slope);
+
+  fitfunc->SetParError(0,0.10 * largesty);
+  fitfunc->SetParError(1,0.25);
+  fitfunc->SetParError(2,0.10 *((largestx*.01)/2.35));
+  fitfunc->SetParError(3,5);
+  fitfunc->SetParError(4,0.5);
+  fitfunc->SetParError(5,0.10 * step);
+  fitfunc->SetParError(6,0.10 * offset);
+  return;
+}
+
+void photo_peak_fit_DoFit(TH1 *fithist, TF1 *fitfunc, Option_t *opt) {
+  TString options = opt;
+  TVirtualFitter::SetMaxIterations(100000);
+
+  bool verbose = !options.Contains("Q");
+  bool noprint =  options.Contains("no-print");
+  if(noprint) {
+    options.ReplaceAll("no-print","");
+  }
+
+  if(fithist->GetSumw2()->fN!=fithist->GetNbinsX()+2)
+    fithist->Sumw2();
+  
+  TFitResultPtr fitres = fithist->Fit(fitfunc,Form("%sLRSME",options.Data()));
+  
+  //fitres.Get()->Print();
+  printf("chi^2/NDF = %.02f\n",fitfunc->GetChisquare()/(double)fitfunc->GetNDF());
+
+  if(!fitres.Get()->IsValid()) {
+    printf("fit has failed, trying refit... ");
+    //SetParameter(3,0.1);
+    //SetParameter(4,0.01);
+    //SetParameter(5,0.0);
+    fithist->GetListOfFunctions()->Last()->Delete();
+    fitres = fithist->Fit(fitfunc,Form("%sLRSME",options.Data())); //,Form("%sRSM",options.Data()))
+    if( fitres.Get()->IsValid() ) {
+      printf(" refit passed!\n");
+    } else {
+      printf(" refit also failed :( \n");
+    }
+  }
+
+  //Double_t binwidth = fithist->GetBinWidth(GetParameter("centroid"));
+  //Double_t width    = TF1::GetParameter("sigma");
+  Double_t xlow,xhigh;
+  //Double_t int_low,int_high;
+  fitfunc->GetRange(xlow,xhigh);
+  //int_low  = xlow - 5.*width;
+  //int_high = xhigh +5.*width;
+
+  TF1 *fbg = new TF1("photo_peak_fit_bg", photo_peak_fit_StepBG, xlow, xhigh, 6);
+  fbg->SetParameter(0,fitfunc->GetParameter(0));
+  fbg->SetParameter(1,fitfunc->GetParameter(1));
+  fbg->SetParameter(2,fitfunc->GetParameter(2));
+  fbg->SetParameter(3,fitfunc->GetParameter(5));
+  fbg->SetParameter(4,fitfunc->GetParameter(6));
+  
+  double fArea = fitfunc->Integral(xlow,xhigh) / fithist->GetBinWidth(1);
+  double bgArea = fbg->Integral(xlow,xhigh) / fithist->GetBinWidth(1);
+  fArea -= bgArea;
+  delete fbg;
+
+  if(xlow>xhigh){
+    //std::swap(xlow,xhigh);
+    Double_t tmp = xlow;
+    xlow = xhigh;
+    xhigh = tmp;
+  }
+
+  double fSum = fithist->Integral(fithist->GetXaxis()->FindBin(xlow),
+                           fithist->GetXaxis()->FindBin(xhigh));// * fithist->GetBinWidth(1);
+  printf("sum between markers: %02f\n",fSum);
+  double fDSum = TMath::Sqrt(fSum);
+  fSum -= bgArea;
+  printf("sum after subtraction: %02f\n",fSum);
+
+  if(!verbose) {
+    printf("hist: %s\n",fithist->GetName());
+    //Print();
+    /*
+    printf("BG Area:         %.02f\n",bgArea);
+    printf("GetChisquared(): %.4f\n", fitfunc->GetChisquare());
+    printf("GetNDF():        %i\n",   fitfunc->GetNDF());
+    printf("GetProb():       %.4f\n", fitfunc->GetProb());*/
+    //TF1::Print();
+  }
+
+  //printf("fithist->GetListOfFunctions()->FindObject(this) = 0x%08x\n",fithist->GetListOfFunctions()->FindObject(GetName()));
+  //fflush(stdout);
+  //Copy(*fithist->GetListOfFunctions()->FindObject(GetName()));
+  //  fithist->GetListOfFunctions()->Add(&fBGFit); //use to be a clone.
+  //fithist->GetListOfFunctions()->Add(fBGFit.Clone()); //use to be a clone.
+
+  //SetParent(0); //fithist);
+
+  //delete tmppeak;
+  return;
+}
+
+void photo_peak_fit(){
+  std::cout << std::endl << "Macro: kobamac/root/photophoto_peak_fit.C" << std::endl;
+  TCanvas* canvas = gPad->GetCanvas();
+  TVirtualPad *sel_pad = canvas->GetPad(gPad->GetNumber());
+  if (sel_pad == 0) {
+    std::cout << "There is no sel_pad." << std::endl;
+    return;
+  }
+  TList *listofpri = sel_pad->GetListOfPrimitives();
+  if (listofpri == 0) {
+    std::cout << "The pad includes nothing." << std::endl;
+    return;
+  }
+  TIter next(listofpri);
+  TObject *obj;
+  TH1 *hist = 0;
+  while (obj = next()){
+    if (obj->InheritsFrom("TH2")) {
+      std::cout << "This script can not handle TH2 histograms." << std::endl;
+      return;
+    }
+    if (obj->InheritsFrom("TH1")) {
+      hist = (TH1*)obj;
+      break;
+    }
+  }
+  if(hist == 0){
+    std::cout << "TH1 histogram was not found in this pad." << std::endl;
+    return;
+  }
+
+  TGraph *gr;
+  while(gr = (TGraph*)listofpri->FindObject("photo_peak_fit_Graph")){
+    gr->Delete();
+  }
+  
+  gPad->SetCrosshair();
+  TGraph *grng = (TGraph*)gPad->WaitPrimitive("Graph","PolyLine");
+  grng->SetName("photo_peak_fit_Graph");
+  Int_t np = grng->GetN();
+  if (np != 2) {
+    std::cout << "Number of points: " << np << std::endl;
+    std::cout << "Number of points should be 2." << std::endl;
+    grng->Delete();
+    gPad->SetCrosshair(0);
+    return;
+  }
+  gPad->SetCrosshair(0);
+  Double_t x0, x1, y0, y1;
+  grng->GetPoint(0,x0,y0);
+  grng->GetPoint(1,x1,y1);
+
+  //TF1 *f = new TF1("photo_peak_fit", photo_peak_fit_PhotoPeakBG, x0, x1, 7);
+  TF1 *f = new TF1("photo_peak_fit", photo_peak_fit_PhotoPeakBG, x0, x1, 7);
+  
+  photo_peak_fit_InitParams(hist, f);
+  photo_peak_fit_DoFit(hist,f,"Q+");
+  
+//  f->SetParameter(0,200.0);
+//  f->SetParameter(1,0.5);
+//  f->SetParameter(2,0.1);
+//  f->SetParameter(3,60.1);
+//  f->SetParameter(4,0.1);
+//  f->SetParameter(5,5.1);
+//  f->SetParameter(6,1.1);
+//  f->Draw("same");
+
+//  TF1 *f2 = new TF1("photo_peak_fit_bg", photo_peak_fit_StepBG, 0., 80., 6);
+  
+//  f2->SetParameter(0,200.0);
+//  f2->SetParameter(1,0.5);
+//  f2->SetParameter(2,0.1);
+//  f2->SetParameter(3,5.1);
+//  f2->SetParameter(4,1.1);
+//  f2->Draw("same");
+
+  canvas->Modified();
+  canvas->Update();
+  //f->Delete();
+
+  return;
+}
