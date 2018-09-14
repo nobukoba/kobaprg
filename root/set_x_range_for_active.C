fe530ab59275e044d51214d376595f0dd6b595a7
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2018-09-14 11:10:15 +0900

    set_x/y_range_for_active.C was modified

---
+++ root/set_x_range_for_active.C
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "TROOT.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TGListTree.h"
+#include "TList.h"
+#include "TKey.h"
+#include "TGInputDialog.h"
+#include "TH1.h"
+
+
+TGListTreeItem *SearchNextItem(TGListTreeItem *cur_item){
+  if(cur_item->GetNextSibling()){
+    return cur_item->GetNextSibling();
+  } else if (cur_item->GetParent()){
+    return SearchNextItem(cur_item->GetParent());
+  }else{
+    return 0;
+  }
+}
+
+TGListTreeItem *NextItem(TGListTreeItem *cur_item){
+  if(cur_item->GetFirstChild()){
+    return cur_item->GetFirstChild();
+  }
+  return SearchNextItem(cur_item);
+}
+
+void set_x_range_for_active(Double_t x0, Double_t x1){
+  TGListTree *hist_fListTree = (TGListTree *) gROOT->ProcessLine("pHistBrowser->GetHistListTree();");
+  if (!hist_fListTree) {return;}
+  TGListTreeItem *cur_ListTreeItem = hist_fListTree->GetFirstItem();
+  while(cur_ListTreeItem){
+    if(cur_ListTreeItem->IsActive()){
+      TObject *userdata = (TObject*)cur_ListTreeItem->GetUserData();
+      if (userdata->InheritsFrom("TKey")){
+      	userdata = ((TKey*)userdata)->ReadObj();
+	cur_ListTreeItem->SetUserData(userdata);
+      }
+      if (userdata->InheritsFrom("TH1")){
+	((TH1*)userdata)->GetXaxis()->SetRangeUser(x0,x1);
+      }
+    }
+    cur_ListTreeItem = NextItem(cur_ListTreeItem);
+  }
+  return;
+}
+
+void set_x_range_for_active(){
+  char command[1024];
+  strlcpy(command, "0.0 1.0",sizeof(command));
+  new TGInputDialog(gClient->GetRoot(),0,
+		    "Enter range: %f %f",
+		    command,command);
+  TString str = command;
+  str.ReplaceAll(","," ");
+  Double_t x0, x1;
+  sscanf(str.Data(),"%lf %lf", &x0, &x1);
+  set_x_range_for_active(x0,x1);
+  return;
+}
