fdc9b42b02f0bbcfb4c959c2f3099eeb3cd81945
temp <temp>
2018-09-21 21:35:43 +0900

    offline/all_dat2root.sh permission

--- offline/all_dat2root.sh
+++ offline/all_dat2root.sh
@@ -1,8 +1,8 @@
-for str in ../data/run*
+for str in ./data/run*
 do
-    str2=${str#../data/run}
+    str2=${str#./data/run}
     if [ ${str2%.dat} -ne 3012 ] ; then
-	echo ../kobamac/offline/offline $str run${str2%.dat}.root
-	../kobamac/offline/offline $str run${str2%.dat}.root
+	echo ./kobamac/offline/offline $str root_files/run${str2%.dat}.root
+	#./kobamac/offline/offline $str run${str2%.dat}.root
     fi
 done
