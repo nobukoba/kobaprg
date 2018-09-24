cb1fd24c24faf231fd2c78f53eb0b542b41546f2
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2018-09-25 04:36:20 +0900

    print_ListTreeItem_status.C was bug-fixed in selecting the first item

--- root/dev/print_ListTreeItem_status.C
+++ root/dev/print_ListTreeItem_status.C
@@ -26,10 +26,16 @@ void print_ListTreeItem_status(){
   if (!hist_fListTree) {return;}
   TGListTreeItem *cur_ListTreeItem = hist_fListTree->GetFirstItem();
   
-  while(cur_ListTreeItem = NextItem(cur_ListTreeItem)){
-    std::cout<< "cur_ListTreeItem GetText: " << cur_ListTreeItem->GetText() <<std::endl;
-    std::cout<< "cur_ListTreeItem IsChecked: " << cur_ListTreeItem->IsChecked() <<std::endl;
-    std::cout<< "cur_ListTreeItem IsActive: " << cur_ListTreeItem->IsActive() <<std::endl;
+  while(cur_ListTreeItem){
+    std::cout << ""  << std::endl;
+    std::cout << "GetText(): " << cur_ListTreeItem->GetText() << std::endl;
+    std::cout << "IsChecked(): " << cur_ListTreeItem->IsChecked() << std::endl;
+    std::cout << "IsActive(): " << cur_ListTreeItem->IsActive() << std::endl;
+    std::cout << "GetUserData(): " << cur_ListTreeItem->GetUserData() << std::endl;
+    std::cout << "GetUserData()->GetName(): " << ((TObject*)cur_ListTreeItem->GetUserData())->GetName() << std::endl;
+    std::cout << "GetUserData()->GetTitle(): " << ((TObject*)cur_ListTreeItem->GetUserData())->GetTitle() << std::endl;
+    std::cout << "GetUserData()->ClassName(): " << ((TObject*)cur_ListTreeItem->GetUserData())->ClassName() << std::endl;
+    cur_ListTreeItem = NextItem(cur_ListTreeItem);
   }
   
   return;
