a496444bfd39d82eeaf9af3dfb1fbd7ce958be95
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2018-09-25 22:26:49 +0900

    root/dev/print_active.C was added

---
+++ root/dev/print_active.C
@@ -0,0 +1,46 @@
+#include "TPad.h"
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TBox.h"
+#include "TGListTree.h"
+
+TGListTreeItem *NextItem(TGListTreeItem *cur_item){
+  if(cur_item->GetFirstChild()){
+    return cur_item->GetFirstChild();
+  }
+  return SearchNextItem(cur_item);
+}
+
+TGListTreeItem *SearchNextItem(TGListTreeItem *cur_item){
+  if(cur_item->GetNextSibling()){
+    return cur_item->GetNextSibling();
+  } else if (cur_item->GetParent()){
+    return SearchNextItem(cur_item->GetParent());
+  }else{
+    return 0;
+  }
+}
+
+void print_ListTreeItem_status(){
+  TGListTree *hist_fListTree = (TGListTree *) gROOT->ProcessLine("pHistBrowser->GetHistListTree();");
+  if (!hist_fListTree) {return;}
+  TGListTreeItem *cur_ListTreeItem = hist_fListTree->GetFirstItem();
+  
+  while(cur_ListTreeItem){
+    if (!cur_ListTreeItem->IsActive()) {
+      cur_ListTreeItem = NextItem(cur_ListTreeItem);
+      continue;
+    }
+    std::cout << ""  << std::endl;
+    std::cout << "GetText(): " << cur_ListTreeItem->GetText() << std::endl;
+    std::cout << "IsChecked(): " << cur_ListTreeItem->IsChecked() << std::endl;
+    std::cout << "IsActive(): " << cur_ListTreeItem->IsActive() << std::endl;
+    std::cout << "GetUserData(): " << cur_ListTreeItem->GetUserData() << std::endl;
+    std::cout << "GetUserData()->GetName(): " << ((TObject*)cur_ListTreeItem->GetUserData())->GetName() << std::endl;
+    std::cout << "GetUserData()->GetTitle(): " << ((TObject*)cur_ListTreeItem->GetUserData())->GetTitle() << std::endl;
+    std::cout << "GetUserData()->ClassName(): " << ((TObject*)cur_ListTreeItem->GetUserData())->ClassName() << std::endl;
+    cur_ListTreeItem = NextItem(cur_ListTreeItem);
+  }
+  
+  return;
+}
