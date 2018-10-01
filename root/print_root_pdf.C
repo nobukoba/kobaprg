6f5f0f765549b85b22a488ff0554bf8e1873826d
nobukoba <n.kobayashi.phd@gmail.com>
2018-10-01 20:40:05 +0900

    initial_working_dir was added in print_root_pdf_open.C print_root_pdf

--- root/print_root_pdf.C
+++ root/print_root_pdf.C
@@ -1,7 +1,11 @@
 void print_root_pdf() {
+  TNamed *named = (TNamed*)gROOT->FindObjectAny("initial_working_dir");
+  if (named) {gSystem->cd(named->GetTitle());}
+  std::cout << "gSystem->pwd(): " << gSystem->pwd() << std::endl;
+
   TString sprinter  = gEnv->GetValue("Print.Printer", "");
   TString sprintCmd = gEnv->GetValue("Print.Command", "lpr -o raw");
-  TString fn = "../../root.pdf";
+  TString fn = "root.pdf";
   TString cmd = sprintCmd;
   if (cmd.Contains("%p"))
     cmd.ReplaceAll("%p", sprinter);
--- root/print_root_pdf_open.C
+++ root/print_root_pdf_open.C
@@ -1,4 +1,8 @@
 void print_root_pdf_open() {
-  gSystem->Exec("acroread ../../root.pdf &");
+  TNamed *named = (TNamed*)gROOT->FindObjectAny("initial_working_dir");
+  if (named) {gSystem->cd(named->GetTitle());}
+  std::cout << "gSystem->pwd(): " << gSystem->pwd() << std::endl;
+  //gSystem->Exec("acroread root.pdf &");
+  gSystem->Exec("evince root.pdf &");
   return;
 }
