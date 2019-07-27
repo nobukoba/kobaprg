ddbd2ac88710725311c23b6322d35bce94315702
nobukoba <n.kobayashi.phd@gmail.com>
2019-07-27 22:49:40 +0900

    modified

--- root/exp/cagragr/merging.C
+++ root/exp/cagragr/merging.C
@@ -1,5 +1,88 @@
-Int_t merging() {
+#include <iostream>
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TROOT.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TGFileBrowser.h"
+#include "TGListTree.h"
+#include "TList.h"
+#include "TKey.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TTree.h"
 
+void CopyDir(TDirectory *source, TDirectory *target) {
+  TKey *key;
+  TIter nextkey(source->GetListOfKeys());
+  while ((key = (TKey*)nextkey())) {
+    const char *classname = key->GetClassName();
+    TClass *cl = gROOT->GetClass(classname);
+    TObject *obj = key->ReadObj();
+    if (!cl) continue;
+    if(target->FindObject(obj->GetName())){
+      continue;
+    }
+    if (cl->InheritsFrom(TDirectory::Class())) {
+      TDirectory * dir = target->mkdir(obj->GetName());
+      CopyDir((TDirectory *)obj,dir);
+    } else if (cl->InheritsFrom(TTree::Class())) {
+      continue;
+      //target->cd();
+      //TTree *T = (TTree*)source->Get(key->GetName());
+      //T->CloneTree(-1,"fast");
+    } else if (cl->InheritsFrom(TH1::Class())){
+      target->cd();
+      obj->Clone(obj->GetName());
+    }
+    delete obj;
+  }
+}
+
+void MergeDir(TDirectory *source, TDirectory *target) {
+  TKey *key;
+  TIter nextkey(source->GetListOfKeys());
+  while ((key = (TKey*)nextkey())) {
+    if(key->GetCycle() > 1){continue;}
+    TObject *tgt = target->FindObject(key->GetName());
+    if (!tgt) {
+      std::cout << "key: " << key->GetName()
+		<< " was not found in the directory: "
+		<< target->GetName()
+		<< std::endl;
+      continue;
+    }
+    const char *classname = key->GetClassName();
+    TClass *cl = gROOT->GetClass(classname);
+    const char *tgtclassname = tgt->ClassName();
+    TClass *tgtcl = gROOT->GetClass(tgtclassname);
+    if (cl != tgtcl) {
+      std::cout << "source class: " << classname
+		<< " is different from target class: "
+		<< tgtclassname
+		<< std::endl;
+      continue;
+    }
+    TObject *obj = key->ReadObj();
+    if (!cl) continue;
+    if (cl->InheritsFrom(TDirectory::Class())) {
+      MergeDir((TDirectory *)obj,(TDirectory *)tgt);
+    } else if (cl->InheritsFrom(TTree::Class())) {
+      continue;
+      //TTree *T = (TTree*)source->Get(tgt->GetName());
+      //T->Add((TTree*)obj);
+    } else if (cl->InheritsFrom(TH2::Class())){
+      ((TH2*)tgt)->Add((TH2*)obj);
+    } else if (cl->InheritsFrom(TH1::Class())){
+      ((TH1*)tgt)->Add((TH1*)obj);
+    }
+    delete obj;
+  }
+}
+
+Int_t merging() {
   const Int_t nfiles = 1024;
   const Int_t nsets  = 4;
   TString filenames[nsets][nfiles];
@@ -21,4 +104,17 @@ Int_t merging() {
       std::cout << "set: " << j << ", " << filenames[j][i] << std::endl;
     }
   }
+  
+  TDirectory *mergedroot = TFile::Open("merged.root","recreate");
+  TDirectory *first_dir  = TFile::Open("run2130/hist_MakeClover_Nobu_20190727_1st.root","read");
+  TDirectory *second_dir = TFile::Open("run2131/hist_MakeClover_Nobu_20190727_1st.root","read");
+  
+  std::cout << std::endl << "Merge start." << std::endl;
+  std::cout << "Base dir: " << first_dir->GetName() << std::endl;
+  CopyDir(first_dir, mergedroot);
+  MergeDir(second_dir, mergedroot);
+  mergedroot->Write();
+  mergedroot->Close();
+  std::cout << "Mearge finished." << std::endl;
+  return;
 }
