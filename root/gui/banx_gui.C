7dfa57407187f4e2fccb71dcde737c3d837d1d9c
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-11-27 00:09:37 +0900

    banx/y_gui.C was modified

--- root/gui/banx_gui.C
+++ root/gui/banx_gui.C
@@ -72,26 +72,6 @@ void banx_gui(){
     std::cout << "There is no gPad. This script is terminated." << std::endl;
     return;
   }
-  TList *listofpri = gPad->GetListOfPrimitives();
-  TIter next(listofpri);
-  TObject *obj;
-  TH2 *hist = 0;
-  while ((obj = next())){
-    if (obj->InheritsFrom("TH2")) {
-      hist = (TH2*)obj;
-      std::cout << "TH2 hist was found." << std::endl;
-      break;
-    }
-  }
-  if(hist == 0){
-    std::cout << "TH2 histogram was not found in this pad. This script is terminated." << std::endl;
-    return;
-  }
-
-  if (!gPad) {
-    std::cout << "There is no gPad. This script is terminated." << std::endl;
-    return;
-  }
   Double_t x0, y0;
   WaitOneClickY *primi = new WaitOneClickY(&x0, &y0); delete primi;
 
