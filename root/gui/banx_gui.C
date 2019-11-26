33a1609acff55d2f3856742933e156e1d4b83b81
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-11-26 23:22:49 +0900

    gui with cui

--- root/gui/banx_gui.C
+++ root/gui/banx_gui.C
@@ -67,7 +67,7 @@ private:
   TCanvas *canvas;
 };
 
-void banx_gui(Double_t par0, Double_t par1){
+void banx_gui(){
   if (!gPad) {
     std::cout << "There is no gPad. This script is terminated." << std::endl;
     return;
@@ -87,57 +87,7 @@ void banx_gui(Double_t par0, Double_t par1){
     std::cout << "TH2 histogram was not found in this pad. This script is terminated." << std::endl;
     return;
   }
-  
-  gROOT->cd();
-  TString str = hist->GetName();
-  str += "_bnx";
-  TString str_n = str;
-  Int_t num = 1;
-  while (gROOT->Get(str_n.Data())) {
-    str_n = Form("%s%d",str.Data(),num);
-    num++;
-  }
-  
-  TString str2 = hist->GetName();
-  str2 += "_cut";
-  TString str2_n = str2;
-  Int_t num2 = 1;
-  while (gROOT->Get(str2_n.Data())) {
-    str2_n = Form("%s%d",str2.Data(),num);
-    num2++;
-  }
-  
-  TH1D *hout = new TH1D(str_n, hist->GetTitle(), hist->GetNbinsX(),
-			hist->GetXaxis()->GetXmin(), hist->GetXaxis()->GetXmax());
-  TH2 *hout2 = (TH2*)hist->Clone(str2_n);
-  hout2->Reset();
-  hout2->SetTitle(hist->GetTitle());
-  
-  Int_t j1 = hist->GetYaxis()->FindBin(par0);
-  Int_t j2 = hist->GetYaxis()->FindBin(par1);
-  Double_t binw = hist->GetYaxis()->GetBinWidth(1);
-  Double_t factor1 = (hist->GetYaxis()->GetBinUpEdge(j1) - par0)  / binw;
-  Double_t factor2 = (par1 - hist->GetYaxis()->GetBinLowEdge(j2)) / binw;
-  Double_t factor;
-  for (Int_t j = j1; j <= j2; j++) {
-    if      (j == j1) {factor = factor1;}
-    else if (j == j2) {factor = factor2;}
-    else              {factor = 1.;}
-    for (Int_t i = 0; i <= hist->GetNbinsX()+1; i++) {
-      hout->Fill(hist->GetXaxis()->GetBinCenter(i),
-		 hist->GetBinContent(i,j)*factor);
-      hout2->Fill(hist->GetXaxis()->GetBinCenter(i),
-		  hist->GetYaxis()->GetBinCenter(j),
-		  hist->GetBinContent(i,j)*factor);
-    }
-  }
-  hout->Draw();
-  gPad->Update();
-  gPad->GetFrame()->SetBit(TBox::kCannotMove);
-  return;
-}
 
-void banx_gui(){
   if (!gPad) {
     std::cout << "There is no gPad. This script is terminated." << std::endl;
     return;
@@ -174,6 +124,8 @@ void banx_gui(){
     y0 = y1;
     y1 = tmpy;
   }
-  banx_gui(y0,y1);
+
+  gROOT->ProcessLine(".L ../cui/banx.C");
+  gROOT->ProcessLine(Form("banx((TH1*)%p,%f,%f)",hist,y0,y1));
   return;
 }
--- root/gui/bany_gui.C
+++ root/gui/bany_gui.C
@@ -65,7 +65,7 @@ private:
   TCanvas *canvas;
 };
 
-void bany_gui(Double_t par0, Double_t par1){
+void bany_gui(){
   if (!gPad) {
     std::cout << "There is no gPad. This script is terminated." << std::endl;
     return;
@@ -85,57 +85,7 @@ void bany_gui(Double_t par0, Double_t par1){
     std::cout << "TH2 histogram was not found in this pad. This script is terminated." << std::endl;
     return;
   }
-  
-  gROOT->cd();
-  TString str = hist->GetName();
-  str += "_bny";
-  TString str_n = str;
-  Int_t num = 1;
-  while (gROOT->Get(str_n.Data())) {
-    str_n = Form("%s%d",str.Data(),num);
-    num++;
-  }
 
-  TString str2 = hist->GetName();
-  str2 += "_cut";
-  TString str2_n = str2;
-  Int_t num2 = 1;
-  while (gROOT->Get(str2_n.Data())) {
-    str2_n = Form("%s%d",str2.Data(),num);
-    num2++;
-  }
-  
-  TH1D *hout = new TH1D(str_n, hist->GetTitle(), hist->GetNbinsY(),
-			hist->GetYaxis()->GetXmin(), hist->GetYaxis()->GetXmax());
-  TH2 *hout2 = (TH2*)hist->Clone(str2_n);
-  hout2->Reset();
-  hout2->SetTitle(hist->GetTitle());
-
-  Int_t i1 = hist->GetXaxis()->FindBin(par0);
-  Int_t i2 = hist->GetXaxis()->FindBin(par1);
-  Double_t binw = hist->GetXaxis()->GetBinWidth(1);
-  Double_t factor1 = (hist->GetXaxis()->GetBinUpEdge(i1) - par0)  / binw;
-  Double_t factor2 = (par1 - hist->GetXaxis()->GetBinLowEdge(i2)) / binw;
-  Double_t factor;
-  for (Int_t i = i1; i <= i2; i++) {
-    if      (i == i1) {factor = factor1;}
-    else if (i == i2) {factor = factor2;}
-    else              {factor = 1.;}
-    for (Int_t j = 0; j <= hist->GetNbinsY()+1; j++) {
-      hout->Fill(hist->GetYaxis()->GetBinCenter(j),
-		 hist->GetBinContent(i,j)*factor);
-      hout2->Fill(hist->GetXaxis()->GetBinCenter(i),
-		  hist->GetYaxis()->GetBinCenter(j),
-		  hist->GetBinContent(i,j)*factor);
-    }
-  }
-  hout->Draw();
-  gPad->Update();
-  gPad->GetFrame()->SetBit(TBox::kCannotMove);
-  return;
-}
-
-void bany_gui(){
   if (!gPad) {
     std::cout << "There is no gPad. This script is terminated." << std::endl;
     return;
@@ -171,7 +121,9 @@ void bany_gui(){
     x0 = x1;
     x1 = tmpx;
   }
-
-  bany_gui(x0,x1);
+  
+  gROOT->ProcessLine(".L ../cui/bany.C");
+  gROOT->ProcessLine(Form("bany((TH1*)%p,%f,%f)",hist,x0,x1));
+  
   return;
 }
