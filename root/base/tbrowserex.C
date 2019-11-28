dec33bb756fc7bae39a3a7a16bc2cd17e9009dea
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-11-28 21:46:18 +0900

    WaitOneClickX/Y were moved to txbrowserex.C

--- root/base/tbrowserex.C
+++ root/base/tbrowserex.C
@@ -44,6 +44,118 @@
 #include "TGraphErrors.h"
 #include "KeySymbols.h"
 
+class WaitOneClickY{
+public:
+  ~WaitOneClickY(){
+    canvas->Disconnect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",this);
+  }
+  WaitOneClickY(Double_t *x_in, Double_t *y_in):
+    x(x_in), y(y_in),
+    fCrosshairPos(0), pxlast(0), pylast(0){
+    canvas = gPad->GetCanvas();
+    canvas->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",
+		    "WaitOneClickY", this, "DrawFunction(Int_t,Int_t,Int_t,TObject*)");
+    canvas->WaitPrimitive("TMarker");
+  }
+  void DrawFunction(Int_t event, Int_t x_in, Int_t y_in, TObject*){
+    if (event == kButton1Down) {
+      pxlast = gPad->GetEventX();
+      pylast = gPad->GetEventY();
+      *x = gPad->AbsPixeltoX(pxlast);
+      *y = gPad->AbsPixeltoY(pylast);
+      mk.PaintMarker(*x, *y);
+      return;
+    }
+    if (event == kMouseEnter) return;
+    canvas->FeedbackMode(kTRUE);
+    //erase old position and draw a line at current position
+    Int_t pxmin,pxmax,pymin,pymax,pxold,pyold,px,py;
+    pxold = fCrosshairPos%10000;
+    pyold = fCrosshairPos/10000;
+    px    = gPad->GetEventX();
+    py    = gPad->GetEventY()+1;
+    pxmin = 0;
+    pxmax = canvas->GetWw();
+    pymin = 0;
+    pymax = gPad->GetWh();
+    //if(pxold) gVirtualX->DrawLine(pxold,pymin,pxold,pymax);
+    if(pyold) gVirtualX->DrawLine(pxmin,pyold,pxmax,pyold);
+    if (event == kButton1Down ||
+	event == kButton1Up   ||
+	event == kMouseLeave) {
+      fCrosshairPos = 0;
+      return;
+    }
+    //gVirtualX->DrawLine(px,pymin,px,pymax);
+    gVirtualX->DrawLine(pxmin,py,pxmax,py);
+    fCrosshairPos = px + 10000*py;
+    return;
+  }
+private:
+  Double_t *x;
+  Double_t *y;
+  Int_t fCrosshairPos;
+  Int_t pxlast, pylast;
+  TMarker mk;
+  TCanvas *canvas;
+};
+
+class WaitOneClickX{
+public:
+  ~WaitOneClickX(){
+    canvas->Disconnect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",this);
+  }
+  WaitOneClickX(Double_t *x_in, Double_t *y_in):
+    x(x_in), y(y_in),
+    fCrosshairPos(0), pxlast(0), pylast(0){
+    canvas = gPad->GetCanvas();
+    canvas->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",
+		    "WaitOneClickX", this, "DrawFunction(Int_t,Int_t,Int_t,TObject*)");
+    canvas->WaitPrimitive("TMarker");
+  }
+  void DrawFunction(Int_t event, Int_t x_in, Int_t y_in, TObject*){
+    if (event == kButton1Down) {
+      pxlast = gPad->GetEventX();
+      pylast = gPad->GetEventY();
+      *x = gPad->AbsPixeltoX(pxlast);
+      *y = gPad->AbsPixeltoY(pylast);
+      mk.PaintMarker(*x, *y);
+      return;
+    }
+    if (event == kMouseEnter) return;
+    canvas->FeedbackMode(kTRUE);
+    //erase old position and draw a line at current position
+    Int_t pxmin,pxmax,pymin,pymax,pxold,pyold,px,py;
+    pxold = fCrosshairPos%10000;
+    pyold = fCrosshairPos/10000;
+    px    = gPad->GetEventX();
+    py    = gPad->GetEventY()+1;
+    pxmin = 0;
+    pxmax = canvas->GetWw();
+    pymin = 0;
+    pymax = gPad->GetWh();
+    if(pxold) gVirtualX->DrawLine(pxold,pymin,pxold,pymax);
+    //if(pyold) gVirtualX->DrawLine(pxmin,pyold,pxmax,pyold);
+    if (event == kButton1Down ||
+	event == kButton1Up   ||
+	event == kMouseLeave) {
+      fCrosshairPos = 0;
+      return;
+    }
+    gVirtualX->DrawLine(px,pymin,px,pymax);
+    //gVirtualX->DrawLine(pxmin,py,pxmax,py);
+    fCrosshairPos = px + 10000*py;
+    return;
+  }
+private:
+  Double_t *x;
+  Double_t *y;
+  Int_t fCrosshairPos;
+  Int_t pxlast, pylast;
+  TMarker mk;
+  TCanvas *canvas;
+};
+
 class TGFileBrowserEx : public TGFileBrowser {
 public:
   TGFileBrowserEx(const TGWindow *p, TBrowser* b, UInt_t w, UInt_t h) : 
--- root/fit/fit_p1g.C
+++ root/fit/fit_p1g.C
@@ -9,62 +9,6 @@
 #include "TMarker.h"
 #include "TString.h"
 
-class WaitOneClickX{
-public:
-  ~WaitOneClickX(){
-    canvas->Disconnect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",this);
-  }
-  WaitOneClickX(Double_t *x_in, Double_t *y_in):
-    x(x_in), y(y_in),
-    fCrosshairPos(0), pxlast(0), pylast(0){
-    canvas = gPad->GetCanvas();
-    canvas->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)",
-		    "WaitOneClickX", this, "DrawFunction(Int_t,Int_t,Int_t,TObject*)");
-    canvas->WaitPrimitive("TMarker");
-  }
-  void DrawFunction(Int_t event, Int_t x_in, Int_t y_in, TObject*){
-    if (event == kButton1Down) {
-      pxlast = gPad->GetEventX();
-      pylast = gPad->GetEventY();
-      *x = gPad->AbsPixeltoX(pxlast);
-      *y = gPad->AbsPixeltoY(pylast);
-      mk.PaintMarker(*x, *y);
-      return;
-    }
-    if (event == kMouseEnter) return;
-    canvas->FeedbackMode(kTRUE);
-    //erase old position and draw a line at current position
-    Int_t pxmin,pxmax,pymin,pymax,pxold,pyold,px,py;
-    pxold = fCrosshairPos%10000;
-    pyold = fCrosshairPos/10000;
-    px    = gPad->GetEventX();
-    py    = gPad->GetEventY()+1;
-    pxmin = 0;
-    pxmax = canvas->GetWw();
-    pymin = 0;
-    pymax = gPad->GetWh();
-    if(pxold) gVirtualX->DrawLine(pxold,pymin,pxold,pymax);
-    //if(pyold) gVirtualX->DrawLine(pxmin,pyold,pxmax,pyold);
-    if (event == kButton1Down ||
-	event == kButton1Up   ||
-	event == kMouseLeave) {
-      fCrosshairPos = 0;
-      return;
-    }
-    gVirtualX->DrawLine(px,pymin,px,pymax);
-    //gVirtualX->DrawLine(pxmin,py,pxmax,py);
-    fCrosshairPos = px + 10000*py;
-    return;
-  }
-private:
-  Double_t *x;
-  Double_t *y;
-  Int_t fCrosshairPos;
-  Int_t pxlast, pylast;
-  TMarker mk;
-  TCanvas *canvas;
-};
-
 void fit_p1g() {
   if (!gPad) {
     std::cout << "There is no gPad. This script is terminated." << std::endl;
