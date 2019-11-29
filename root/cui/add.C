55f921685882f5e5dca6b9a67434a8972107f2d2
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-11-29 17:17:29 +0900

    clone_with_suffix

--- root/cui/add.C
+++ root/cui/add.C
@@ -1,9 +1,9 @@
-TH1* add(TH1 *h1, TH1 *h2, Double_t c1, Double_t c2){
+TH1* add(TH1 *hist, TH1 *hist2, Double_t c1, Double_t c2){
   gROOT->ProcessLine(".L ../cui/clone_with_suffix.C");
-  TH1 *hadded = (TH1*)gROOT->ProcessLine(Form("clone_with_suffix((TH1*)%p,\"%s\");",h1,"_add"));
-  hadded->SetTitle(h1->GetTitle());
+  TH1 *hadded = (TH1*)gROOT->ProcessLine(Form("clone_with_suffix((TH1*)%p,\"%s\");",hist,"_add"));
+  hadded->SetTitle(hist->GetTitle());
   hadded->Scale(c1);
-  hadded->Add(h2,c2);
+  hadded->Add(hist2,c2);
   return hadded;
 }
 
--- root/cui/sub.C
+++ root/cui/sub.C
@@ -1,10 +1,9 @@
-TH1* sub(TH1 *h1, TH1 *h2, Double_t c1, Double_t c2){
+TH1* sub(TH1 *hist, TH1 *hist2, Double_t c1, Double_t c2){
   gROOT->ProcessLine(".L ../cui/clone_with_suffix.C");
   TH1 *hsub = (TH1*)gROOT->ProcessLine(Form("clone_with_suffix((TH1*)%p,\"%s\");",hist,"_sub"));
-  hsub->SetTitle(h1->GetTitle());
+  hsub->SetTitle(hist->GetTitle());
   hsub->Scale(c1);
-  hsub->Add(h2,-c2);
-  save->cd();
+  hsub->Add(hist2,-c2);
   return hsub;
 }
 
