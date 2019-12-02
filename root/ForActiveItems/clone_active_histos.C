1532779ada8b8e08ea24a0eb6deec1bf132f49f8
Nobuyuki Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-12-02 12:41:09 +0900

    clone_active_histos.C was added

---
+++ root/ForActiveItems/clone_active_histos.C
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "TROOT.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TGListTree.h"
+#include "TList.h"
+#include "TKey.h"
+#include "TH1.h"
+
+void clone_active_histos(){
+  TBrowserEx *gBrowserEx = (TBrowserEx *)gROOT->ProcessLine("gBrowserEx;");
+  if (!gBrowserEx) {return;}
+  TIter next(gBrowserEx->GetListOfOrderedActiveHistos());
+  TH1 * hist;
+  gROOT->ProcessLine(".L ../cui/clone.C");
+  while((hist = (TH1*)next())){
+    gROOT->ProcessLine(Form("clone((TH1*)%p)",hist));
+  }
+  return;
+}
