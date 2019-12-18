10afbd780a588c46f9b73ea812bacc27708374ed
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2019-12-18 21:13:39 +0900

    start stop auto_reading.C were modified

--- root/timer/start_auto_reading.C
+++ root/timer/start_auto_reading.C
@@ -1,8 +1,6 @@
 #include "TROOT.h"
 
 void start_auto_reading(){
-  if (gROOT->FindObjectAny("shm_flag")){
-    gROOT->ProcessLine("MyTFile::GetShmTimer()->Start(1000,kFALSE);");
-  }
+  gROOT->ProcessLine("MyTFile::StartConversion();");
   return;
 }
