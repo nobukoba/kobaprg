66feb96cdcfaf11c2b7a50fbdfa4039be8af8ff9
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-01-28 10:50:03 +0900

    qstat updated for qstat_M_out parameter

--- sh/qstat_class_mem_cpu.sh
+++ sh/qstat_class_mem_cpu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+qstat_M_out=`qstat -M`
 que_list=($(qstat -Q|awk 'NR>=3{print $1}' ))
 echo "Queue Tot Que Run  Mem Que  Mem Run ncpu Que ncpu Run"
 echo "----- --- --- --- -------- -------- -------- --------"
 for que_name in ${que_list[@]}
 do
-    qstat -M | awk -v que_name=${que_name} 'NR>=6 && que_name==$3 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("%-5s %3d %3d %3d %5d GB %5d GB %8d %8d\n", que_name, tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
+    echo "${qstat_M_out}" | awk -v que_name=${que_name} 'NR>=6 && que_name==$3 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("%-5s %3d %3d %3d %5d GB %5d GB %8d %8d\n", que_name, tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
 done
 echo "----- --- --- --- -------- -------- -------- --------"
-qstat -M | awk 'NR>=6 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("Total %3d %3d %3d %5d GB %5d GB %8d %8d\n", tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
+echo "${qstat_M_out}" | awk 'NR>=6 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("Total %3d %3d %3d %5d GB %5d GB %8d %8d\n", tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
--- sh/qstat_user_mem_cpu.sh
+++ sh/qstat_user_mem_cpu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-user_name_list=($(qstat -M | awk 'NR>=6{print $0}' | sort -k2 | awk '(prev_name!=$2){print $2;prev_name=$2}'))
+qstat_M_out=`qstat -M`
+user_name_list=($(echo "${qstat_M_out}" | awk 'NR>=6{print $0}' | sort -k2 | awk '(prev_name!=$2){print $2;prev_name=$2}'))
 echo "Username Tot Que Run  Mem Que  Mem Run ncpu Que ncpu Run"
 echo "-------- --- --- --- -------- -------- -------- --------"
 for user_name in ${user_name_list[@]}
 do
-    qstat -M | awk -v user_name=${user_name} 'NR>=6  && user_name==$2{gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("%-8s %3d %3d %3d %5d GB %5d GB %8d %8d\n", user_name, tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
+    echo "${qstat_M_out}" | awk -v user_name=${user_name} 'NR>=6  && user_name==$2{gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("%-8s %3d %3d %3d %5d GB %5d GB %8d %8d\n", user_name, tot, que, run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
 done
 echo "-------- --- --- --- -------- -------- -------- --------"
-qstat -M | awk 'NR>=6 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("Total    %3d %3d %3d %5d GB %5d GB %8d %8d\n",tot, que,run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
+echo "${qstat_M_out}" | awk 'NR>=6 {gsub("mw","",$8); tot+=1; if($10=="Q"){que+=1;qmem+=$8;qcpus+=$7;}; if($10=="R"){run+=1;rmem+=$8;rcpus+=$7;}} END {printf("Total    %3d %3d %3d %5d GB %5d GB %8d %8d\n",tot, que,run, qmem*8/1024, rmem*8/1024, qcpus, rcpus);}'
