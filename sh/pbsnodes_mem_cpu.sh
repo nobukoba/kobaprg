ca6a424010506b5c230ee41626573b5100becd8f
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-02-01 23:57:38 +0900

    pbsnodes_mem_cpu.sh was updated

--- sh/pbsnodes_mem_cpu.sh
+++ sh/pbsnodes_mem_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pbsnodes_a_out=`cat pbsnodes_a_20200201_0040.txt`
-#pbsnodes_a_out=`pbsnodes -a`
+#pbsnodes_a_out=`cat pbsnodes_a_20200201_0040.txt`
+pbsnodes_a_out=`pbsnodes -a`
 
 nodes=($(echo "${pbsnodes_a_out}" | grep "Mom = " | awk '{print $3}' | sort))
 for node in ${nodes[@]}
@@ -11,17 +11,22 @@ do
     tmem=`echo  "${str}" | grep "resources_available.mem = " | awk '{sub("kb","",$3);print $3}'`
     tncpu=`echo "${str}" | grep "resources_available.ncpus = " | awk '{print $3}'`
     
-    amem=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "resources_assigned.mem = " | awk '{sub("kb","",$3);print $3}'`
-    ancpu=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "resources_assigned.ncpus = " | awk '{print $3}'`
-    ajobs=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "jobs = " | awk '{sub("jobs = ","",$0);gsub(",","\n",$0);print $0}'| awk '{gsub("/.*","",$1);print $1}' | sort | uniq | wc -l`
+    amem=`echo "${str}" | grep "resources_assigned.mem = " | awk '{sub("kb","",$3);print $3}'`
+    ancpu=`echo "${str}" | grep "resources_assigned.ncpus = " | awk '{print $3}'`
+    ajobs=`echo "${str}" | grep "jobs = " | awk '{sub("jobs = ","",$0);gsub(",","\n",$0);print $0}'| awk '{gsub("/.*","",$1);print $1}' | sort | uniq | wc -l`
     rmem=$((${tmem}-${amem}))
     rncpu=$((${tncpu}-${ancpu}))
+    if [[ "${stat}" = "offline" ||  "${stat}" = "down" ]]
+    then
+	rmem=0
+	rncpu=0
+    fi
     rslt="${rslt}\n${node} ${stat} ${tmem} ${amem} ${rmem} ${tncpu} ${ancpu} ${rncpu} ${ajobs}"
 done
-echo "------------ -------- Memory ------- ---- ncpu ---- ----"
-echo "Batch nodes    Total    Used    Free  Tot Used Free Jobs"
-echo "------------ ------- ------- ------- ---- ---- ---- ----"
+echo "--- -------- -------- Memory ------- ---- ncpu ---- ----"
+echo "Nod Status     Total    Used    Free  Tot Used Free Jobs"
+echo "--- -------- ------- ------- ------- ---- ---- ---- ----"
 echo -e "${rslt}" | awk '(NR>=2){printf("%s %-7s  %4d GB %4d GB %4d GB %4d %4d %4d %4d\n",$1,$2,$3/1024/1024,$4/1024/1024,$5/1024/1024,$6,$7,$8,$9)}'
 echo "------------ ------- ------- ------- ---- ---- ---- ----"
 echo -e "${rslt}" | awk '($2=="busy"||$2=="free"){tmem+=$3;amem+=$4;rmem+=$5;tncpu+=$6;ancpu+=$7;rncpu+=$8;ajobs+=$9}END{printf("Online total %4d GB %4d GB %4d GB %4d %4d %4d %4d\n",tmem/1024/1024,amem/1024/1024,rmem/1024/1024,tncpu,ancpu,rncpu,ajobs)}'
-echo -e "${rslt}" | awk '(NR>=2){tmem+=$3;amem+=$4;rmem+=$5;tncpu+=$6;ancpu+=$7;rncpu+=$8;ajobs+=$9}END{printf("Total        %4d GB                 %4d\n",tmem/1024/1024,tncpu)}'
+echo -e "${rslt}" | awk '(NR>=2){tmem+=$3;amem+=$4;rmem+=$5;tncpu+=$6;ancpu+=$7;rncpu+=$8;ajobs+=$9}END{printf("Total        %4d GB %4d GB         %4d %4d      %4d\n",tmem/1024/1024,amem/1024/1024,tncpu,ancpu,ajobs)}'
