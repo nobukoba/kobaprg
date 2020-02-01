72216b8e68224cc423e774dac65ae243bba3a48e
nobukoba <n.kobayashi.phd@gmail.com>
2020-02-01 23:12:54 +0900

    pbsnodes_mem_cpu.sh was updated

--- sh/pbsnodes_mem_cpu.sh
+++ sh/pbsnodes_mem_cpu.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-#pbsnodes_a_out=`cat pbsnodes_a_20200201_0040.txt`
-pbsnodes_a_out=`pbsnodes -a`
+pbsnodes_a_out=`cat pbsnodes_a_20200201_0040.txt`
+#pbsnodes_a_out=`pbsnodes -a`
 
 nodes=($(echo "${pbsnodes_a_out}" | grep "Mom = " | awk '{print $3}' | sort))
 for node in ${nodes[@]}
 do
-    stat=`echo "${pbsnodes_a_out}" | grep -A 2 "Mom = ${node}" | awk '(NR==3){sub("job-","",$3);sub(",offline","",$3);sub("state-","",$3);print $3}'`
-    tmem=`echo "${pbsnodes_a_out}" | grep -A 10 "Mom = ${node}" | sed '/jobs = /d' | awk '(NR==8){sub("kb","",$3);print $3}'`
-    tncpu=`echo "${pbsnodes_a_out}" | grep -A 10 "Mom = ${node}" | sed '/jobs = /d' | awk '(NR==9){print $3}'`
-
-    if [[ "${stat}" = "free" || "${stat}" = "busy" ]]
-    then
-	amem=`echo "${pbsnodes_a_out}" | grep -A 13 "Mom = ${node}" | awk '(NR==14){sub("kb","",$3);print $3}'`
-	ancpu=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | awk '(NR==16){print $3}'`
-	ajobs=`echo "${pbsnodes_a_out}" | grep -A 4 "Mom = ${node}" | awk '(NR==5){sub("jobs = ","",$0);gsub(",","\n",$0);print $0}'| awk '{gsub("/.*","",$1);print $1}' | sort | uniq | wc -l`
-	rmem=$((${tmem}-${amem}))
-	rncpu=$((${tncpu}-${ancpu}))
-    else
-	ancpu=0
-	amem=0	
-	ajobs=0
-	rncpu=0
-	rmem=0
-    fi
-    #if [[ "${stat}" = "busy" ]]
-    #then
-    #	rmem=0
-    #	amem=${tmem}
-    #fi
+    str=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}"`
+    stat=`echo  "${str}" | grep "state = " | awk '{sub("job-","",$3);sub(",offline","",$3);sub("state-","",$3);print $3}'`
+    tmem=`echo  "${str}" | grep "resources_available.mem = " | awk '{sub("kb","",$3);print $3}'`
+    tncpu=`echo "${str}" | grep "resources_available.ncpus = " | awk '{print $3}'`
+    
+    amem=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "resources_assigned.mem = " | awk '{sub("kb","",$3);print $3}'`
+    ancpu=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "resources_assigned.ncpus = " | awk '{print $3}'`
+    ajobs=`echo "${pbsnodes_a_out}" | grep -A 15 "Mom = ${node}" | grep "jobs = " | awk '{sub("jobs = ","",$0);gsub(",","\n",$0);print $0}'| awk '{gsub("/.*","",$1);print $1}' | sort | uniq | wc -l`
+    rmem=$((${tmem}-${amem}))
+    rncpu=$((${tncpu}-${ancpu}))
     rslt="${rslt}\n${node} ${stat} ${tmem} ${amem} ${rmem} ${tncpu} ${ancpu} ${rncpu} ${ajobs}"
 done
 echo "------------ -------- Memory ------- ---- ncpu ---- ----"
