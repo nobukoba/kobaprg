49bafbed7f85f6caeaf9386b741dfe910d01b56e
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-02-02 01:18:10 +0900

    pbsnodes_avail.sh was added

--- sh/pbsnodes_avail.sh
+++ sh/pbsnodes_avail.sh
@@ -22,10 +22,31 @@ do
     fi
     rslt="${rslt}\n${node} ${stat} ${ajobs} ${tmem} ${amem} ${rmem} ${tncpu} ${ancpu} ${rncpu}"
 done
+
+if [ "$1" = ""  ]
+then
+    umem=1
+else
+    umem=$1
+fi
+if [ "$2" = ""  ]
+then
+    uncpu=1
+else
+    uncpu=$2
+fi
+echo "--------------------------------------------------------"
+echo "Usage: pbsnodes_avail.sh [Memory in GB] [ncpu]          "
+echo "--------------------------------------------------------"
+echo "Show available nodes with free memory > ${umem} & free npcu > ${uncpu}"
+echo "--------------------------------------------------------"
 echo "--- -------- ---- -------- Memory ------- ---- ncpu ----"
 echo "Nod Status   Jobs   Total    Used    Free  Tot Used Free"
 echo "--- -------- ---- ------- ------- ------- ---- ---- ----"
-echo -e "${rslt}" | awk '(NR>=2){printf("%s %-7s  %4d %4d GB %4d GB %4d GB %4d %4d %4d\n",$1,$2,$3,$4/1024/1024,$5/1024/1024,$6/1024/1024,$7,$8,$9)}'
-echo "------------ ---- ------- ------- ------- ---- ---- ----"
-echo -e "${rslt}" | awk '($2=="busy"||$2=="free"){ajobs+=$3;tmem+=$4;amem+=$5;rmem+=$6;tncpu+=$7;ancpu+=$8;rncpu+=$9}END{printf("Online total %4d %4d GB %4d GB %4d GB %4d %4d %4d\n",ajobs,tmem/1024/1024,amem/1024/1024,rmem/1024/1024,tncpu,ancpu,rncpu)}'
-echo -e "${rslt}" | awk '(NR>=2){ajobs+=$3;tmem+=$4;amem+=$5;rmem+=$6;tncpu+=$7;ancpu+=$8;rncpu+=$9}END{printf("Total        %4d %4d GB %4d GB         %4d %4d    \n",ajobs,tmem/1024/1024,amem/1024/1024,tncpu,ancpu)}'
+rslt2=`echo -e "${rslt}" | awk  -v umem=${umem} -v uncpu=${uncpu} '(NR>=2&&($6/1024/1024)>=umem&&$9>=uncpu){printf("%s %-7s  %4d %4d GB %4d GB %4d GB %4d %4d %4d\n",$1,$2,$3,$4/1024/1024,$5/1024/1024,$6/1024/1024,$7,$8,$9)}'`
+if [[ `echo "${rslt2}"| wc -l` = 1 ]]
+then
+    echo "No available nodes"
+else
+    echo "${rslt2}"    
+fi
