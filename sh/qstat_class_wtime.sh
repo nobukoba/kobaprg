7e5e8a2b16d44d7df8a176afec77792efbe9cae4
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2020-02-02 22:35:33 +0900

    qstat_wtime.sh etc were updated

--- sh/qstat_class_wtime.sh
+++ sh/qstat_class_wtime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-qstat_f_out=`cat qstat_f_20200202_1457.txt`
-#qstat_f_out=`qstat -f`
+#qstat_f_out=`cat qstat_f_20200202_1457.txt`
+qstat_f_out=`qstat -f`
 
 qstat_f_out=`echo "${qstat_f_out}" | awk '($0~"Job Id: ")||($0~"job_state = ")||($0~" qtime = ")||($0~" stime = ")||($0~"Job_Owner = ")||($0~"queue = "){print $0}'`
 qstat_f_out=`echo "${qstat_f_out}" | awk '{if($0~" qtime = "){sub("qtime = ","",$0);str="date --date=\"" $0 "\" +%s";printf("qtime = ");system(str)}else if($0~" stime = "){sub("stime = ","",$0);str="date --date=\"" $0 "\" +%s";printf("stime = ");system(str)}else{print $0}}'`
@@ -11,8 +11,8 @@ qstat_f_out=`echo "${qstat_f_out}" | awk -v cdate=${cdate} '{if($3 == "Q"){print
 qstat_f_out=`echo "${qstat_f_out}" | awk '(NR>=2){sub("@.*","",$2);printf("%s %d\n",$0,$6-$5)}'`
 #echo "${qstat_f_out}"
 
-qstat_Q_out=`cat qstat_Q.txt`
-#qstat_Q_out=`qstat -Q`
+#qstat_Q_out=`cat qstat_Q.txt`
+qstat_Q_out=`qstat -Q`
 arr=($(echo "${qstat_Q_out}"|awk 'NR>=3{print $1}'))
 echo "Queue Tot Que Run Q Wait av R Wait av"
 echo "----- --- --- --- --------- ---------"
--- sh/qstat_wtime.sh
+++ sh/qstat_wtime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-qstat_f_out=`cat qstat_f_20200202_1457.txt`
-#qstat_f_out=`qstat -f`
+#qstat_f_out=`cat qstat_f_20200202_1457.txt`
+qstat_f_out=`qstat -f`
 
 qstat_f_out=`echo "${qstat_f_out}" | awk '($0~"Job Id: ")||($0~"job_state = ")||($0~" qtime = ")||($0~" stime = ")||($0~"Job_Owner = ")||($0~"queue = "){print $0}'`
 qstat_f_out=`echo "${qstat_f_out}" | awk '{if($0~" qtime = "){sub("qtime = ","",$0);str="date --date=\"" $0 "\" +%s";printf("qtime = ");system(str)}else if($0~" stime = "){sub("stime = ","",$0);str="date --date=\"" $0 "\" +%s";printf("stime = ");system(str)}else{print $0}}'`
