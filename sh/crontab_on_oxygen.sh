6a39c602dcde0e02bd922a39ee24fc89f54ffda6
nobukoba <n.kobayashi.phd@gmail.com>
2020-06-22 23:33:04 +0900

    '-' for grep was removed

--- sh/crontab_on_oxygen.sh
+++ sh/crontab_on_oxygen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh_fwd_cmd="ssh -fgNL 8083:localhost:8083 cagragr@miho-1"
-ps_grep=`ps aux | grep ssh | grep '-fgNL' | grep 8083 | grep 'cagragr@miho-1' | grep -v grep`
+ps_grep=`ps aux | grep ssh | grep fgNL | grep 8083 | grep 'cagragr@miho-1' | grep -v grep`
 if [ "${ps_grep}" != "" ]; then
     echo "ssh -fgNL 8083:localhost:8083 cagragr@miho-1 is already running"
 else
@@ -9,7 +9,7 @@ else
     eval "${ssh_fwd_cmd}"
 fi
 ssh_fwd_cmd="ssh -fgNL 8084:localhost:8084 cagragr@miho-2"
-ps_grep=`ps aux | grep ssh | grep '-fgNL' | grep 8084 | grep 'cagragr@miho-2' | grep -v grep`
+ps_grep=`ps aux | grep ssh | grep fgNL | grep 8084 | grep 'cagragr@miho-2' | grep -v grep`
 if [ "${ps_grep}" != "" ]; then
     echo "ssh -fgNL 8084:localhost:8084 cagragr@miho-2 is already running"
 else
